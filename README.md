abc1beda6eba33bead07e683fc976c2e189f21db
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-01-14 16:52:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blaze Nest
+
+Blaze Nest is a platform designed to address the housing and entrepreneurial needs of
+students. The project aims to simplify the lodging search for students and provide a
+marketplace for student vendors to showcase their products and services.
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-between items-center">
+      <h1 className="font-bold text-2xl">Welcome to the Blaze Nest Website</h1>
+      <p>
+        Blaze Nest is a platform designed to address the housing and
+        entrepreneurial needs of students. The project aims to simplify the
+        lodging search for students and provide a marketplace for student
+        vendors to showcase their products and services.
+      </p>
+    </div>
+  );
+}
+
+export default App;
