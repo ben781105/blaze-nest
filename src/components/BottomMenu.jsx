546835900a8c17891e9f028b9554371938e44146
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-06-10 09:51:04 +0100

    refactor: I made the bottom menu component reusable

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,64 +1,25 @@
 import { Link, useLocation } from "react-router-dom";
-import {
-  HomeIcon,
-  SavedIcon,
-  CartIcon,
-  MessageIcon,
-  ProfileIcon,
-} from "./icons/index";
 
-const data = [
-  { 
-    id: 1, 
-    path: "/student-dashboard/home", 
-    icon: <HomeIcon className="h-6 w-6" 
-    /> 
-  },
-  { 
-    id: 2, 
-    path: "/student-dashboard/saved",
-    icon: <SavedIcon className="h-6 w-6"
-    /> 
-  },
-  { id: 3,
-    path: "/student-dashboard/cart",
-    icon: <CartIcon className="h-6 w-6"
-    />
-  },
-  { 
-    id: 4,
-    path: "/student-dashboard/messages",
-    icon: <MessageIcon className="h-6 w-6"
-    />
-  },
-  { id: 5,
-    path: "/student-dashboard/profile",
-    icon: <ProfileIcon className="h-6 w-6"
-    /> 
-  },
-];
-
-const BottomMenu = () => {
+const BottomMenu = ({ menuData }) => {
   const location = useLocation();
 
   return (
     <footer className="bg-white fixed bottom-0 w-full h-20 z-50">
       <nav className="flex justify-around p-4">
         <ul className="flex justify-around w-full">
-          {data.map((item) => {
+          {menuData.map((item, index) => {
             const isActive = location.pathname === item.path;
 
             return (
               <li
-                key={item.id}
+                key={index}
                 className={`flex items-center  ${
-                  isActive ? "bg-primary px-3 py-3 text-black" : " px-3 py-3 text-gray-400"
+                  isActive
+                    ? "bg-primary px-3 py-3 text-black"
+                    : " px-3 py-3 text-gray-400"
                 }  rounded-full`}
               >
-                <Link
-                  to={item.path}
-                  className="flex flex-col items-center"
-                >
+                <Link to={item.path} className="flex flex-col items-center">
                   {item.icon}
                 </Link>
               </li>
